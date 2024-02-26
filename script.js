14cf2bbbede4fc6c5ac2f18a5e362e9877ed0314
kaliglass <146648840+kaliglass@users.noreply.github.com>
2024-02-26 16:36:05 +0530

    Added the playRound() function to play one round of RPS

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ function getComputerChoice(){
     return (pick[Math.floor(Math.random() * pick.length)]);
 }
 
+function playRound(playerSelection,computerSelection){
+    if(playerSelection==='rock'){
+        if(computerSelection==='scissors'){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else if(computerSelection==='paper'){
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        else{
+            return `${playerSelection} and ${computerSelection} is a tie`;
+        }
+    }
+
+    else if(playerSelection==='scissors'){
+        if(computerSelection==='rock'){
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        else if(computerSelection==='paper'){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else{
+            return `${playerSelection} and ${computerSelection} is a tie`;
+        }
+    }
+
+    else if(playerSelection==='paper'){
+        if(computerSelection==='scissors'){
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        else if(computerSelection==='rock'){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else{
+            return `${playerSelection} and ${computerSelection} is a tie`;
+        }
+    }
+}
