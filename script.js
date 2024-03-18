358917725e13daf319b4a606ec0afd6afa5c2506
kaliglass <146648840+kaliglass@users.noreply.github.com>
2024-03-18 19:32:22 +0530

    Finished with a simple UI

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let plyrcnt = 0;
 let compcnt = 0;
 
+const resultsDiv = document.createElement("div");
+document.body.appendChild(resultsDiv);
+
 function getComputerChoice(){
     const pick=['rock','paper','scissors'];
      return (pick[Math.floor(Math.random() * pick.length)]);
@@ -10,61 +13,84 @@ function playRound(playerSelection,computerSelection){
     if(playerSelection==='rock'){
         if(computerSelection==='scissors'){
             plyrcnt++;
-             console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+             resultsDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
         }
         else if(computerSelection==='paper'){
             compcnt++;
-             console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+             resultsDiv.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
         }
         else{
-             console.log(`${playerSelection} and ${computerSelection} is a tie`);
+            resultsDiv.textContent =`${playerSelection} and ${computerSelection} is a tie`;
         }
     }
 
     else if(playerSelection==='scissors'){
         if(computerSelection==='rock'){
             compcnt++;
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            resultsDiv.textContent =(`You lose! ${computerSelection} beats ${playerSelection}`);
         }
         else if(computerSelection==='paper'){
             plyrcnt++;
-             console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            resultsDiv.textContent =(`You win! ${playerSelection} beats ${computerSelection}`);
         }
         else{
-             console.log(`${playerSelection} and ${computerSelection} is a tie`);
+            resultsDiv.textContent =(`${playerSelection} and ${computerSelection} is a tie`);
         }
     }
 
     else if(playerSelection==='paper'){
         if(computerSelection==='scissors'){
             compcnt++;
-             console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            resultsDiv.textContent =(`You lose! ${computerSelection} beats ${playerSelection}`);
         }
         else if(computerSelection==='rock'){
             plyrcnt++;
-             console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            resultsDiv.textContent =(`You win! ${playerSelection} beats ${computerSelection}`);
         }
         else{
-             console.log(`${playerSelection} and ${computerSelection} is a tie`);
+             resultsDiv.textContent = (`${playerSelection} and ${computerSelection} is a tie`);
         }
     }
-}
 
-function playGame(){
-    for(let i = 0;i<5;i++){
-        playerSelection = prompt("Enter your choice: ").toLowerCase();
-        computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
+    resultsDiv.textContent+=` \nPlayer:${plyrcnt} - Computer:${compcnt}`;
+
+    if(plyrcnt>=5){
+        resultsDiv.textContent += " \n Congrats! You win the game!";
     }
-    if(plyrcnt>compcnt){
-        console.log(`You win the game with the score of ${plyrcnt}-${compcnt}`);
-    }
-    else if(compcnt>plyrcnt){  
-        console.log(`You took an L,the score is ${plyrcnt}-${compcnt}`);
-    }
-    else{
-        console.log(`It is a tie with the score of ${plyrcnt}-${compcnt}`);
+
+    else if(compcnt>=5){
+        resultsDiv.textContent += " \n You took an L, better luck next time";
     }
 }
 
-playGame();
+
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    let playerSelection = button.textContent.toLowerCase();
+    
+    button.addEventListener("click",()=>{
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection)});
+})
+
+// function playGame(){
+//     for(let i = 0;i<5;i++){
+//         playerSelection = prompt("Enter your choice: ").toLowerCase();
+//         computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection,computerSelection));
+//     }
+//     if(plyrcnt>compcnt){
+//         console.log(`You win the game with the score of ${plyrcnt}-${compcnt}`);
+//     }
+//     else if(compcnt>plyrcnt){  
+//         console.log(`You took an L,the score is ${plyrcnt}-${compcnt}`);
+//     }
+//     else{
+//         console.log(`It is a tie with the score of ${plyrcnt}-${compcnt}`);
+//     }
+// }
+
+// playGame();
